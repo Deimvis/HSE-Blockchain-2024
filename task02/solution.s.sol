995b5943cfe8074607db9456e47b1d9a925ee35d
Dmitriy Brusenin <star_maps@mail.ru>
2024-03-10 01:24:57 +0300

    update task02

---
+++ task02/solution.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract Solution is Script {
+    address payable hacker = payable(0xADB99998d6eD41b6a09C3155085463f54DE888D2);
+    address payable me = payable(0x92199EeC69Dd43c04103e49D3446052117A12C8a);
+
+    function setUp() public {}
+
+    function run() public {
+        uint pk = vm.envUint("HACKER_PRIVATE_KEY");
+        vm.startBroadcast(pk);
+
+        console.log(hacker.balance);
+        me.transfer(0.0009 ether);
+        console.log(hacker.balance);
+
+        vm.stopBroadcast();
+    }
+}
